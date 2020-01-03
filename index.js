baab0b20a79972d0141334ace50f31c77d9b69b0
looplar <looplar@gmx.de>
2020-01-03 10:47:43 +0000

    sfd

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.post('/esp8266_trigger', function(req, res){
 
     dewpoint = xdp.Calc(temperature, humidity).dp;
     console.log(dewpoint);
-    if (dewpoint == NaN) {
+ {
         dewpoint = 0.0;
         console.log(dewpoint);
     }
